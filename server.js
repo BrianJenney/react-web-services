bf752a3e5170bbaa1c36a76bd12cb6c5c223c57a
BrianJenney <brianjenney83@gmail.com>
2017-11-19 21:55:28 -0800

    remove cloudinary config

--- server.js
+++ server.js
@@ -10,12 +10,6 @@ const uri = keys.mongo;
 
 mongoose.connect(uri);
 
-cloudinary.config({
-  cloud_name: '',
-  api_key: '',
-  api_secret: ''
-})
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
