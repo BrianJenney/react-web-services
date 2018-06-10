ca6a716f5845f0b26f08447350fb5fb9d3039461
jenneyb <brianjenney83@gmail.com>
2018-06-10 10:37:35 -0700

    Revert "file upload bug fix"

--- routes/auth/authVerify.js
+++ routes/auth/authVerify.js
@@ -1,25 +1,18 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken')
 
 function verifyToken(req, res, next) {
-    const token = req.headers["authorization"];
-    console.log(req.headers);
-    if (!token) {
-        return res
-            .status(403)
-            .send({ auth: false, message: "No token provided." });
+  const token = req.headers['authorization']
+  console.log(token);
+  if (!token) {
+    return res.status(403).send({ auth: false, message: 'No token provided.' })
+  }
+  jwt.verify(token, 'secret', (err, decoded) => {
+    if (err) {
+      return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' })
     }
-    jwt.verify(token, "secret", (err, decoded) => {
-        if (err) {
-            return res
-                .status(500)
-                .send({
-                    auth: false,
-                    message: "Failed to authenticate token."
-                });
-        }
-        // if everything good, save to request for use in other routes
-        req.userId = decoded._id;
-        next();
-    });
+    // if everything good, save to request for use in other routes
+    req.userId = decoded._id
+    next()
+  })
 }
-module.exports = verifyToken;
+module.exports = verifyToken
