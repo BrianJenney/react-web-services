b635f99d396cd5a34c5472a6aac7523594c2179d
BrianJenney <brianjenney83@gmail.com>
2018-10-25 11:00:42 -0700

    send jwt and user info when registering a user

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,27 +22,7 @@ module.exports = {
         db.User.findOne({ email: req.body.email }).then(user => {
             if (user !== null) {
                 if (user.validPassword(req.body.password)) {
-                    let token = jwt.sign(
-                        {
-                            data: {
-                                email: user.email,
-                                password: user.password,
-                                _id: user._id
-                            }
-                        },
-                        "secret",
-                        { expiresIn: "365 days" }
-                    );
-
-                    const userInfo = {
-                        email: user.email,
-                        userType: user.userType,
-                        _id: user._id,
-                        profilePic: user.profilePic,
-                        phone: user.phoneNumber
-                    };
-
-                    res.json({ userInfo, token: token });
+                    sendJwt(user, res);
                 } else {
                     res.json({
                         errors: "invalid password",
@@ -74,10 +54,9 @@ module.exports = {
         //  hash password
         newUser.password = newUser.generateHash(req.body.password);
 
-        newUser
-            .save()
-            .then(doc => res.json(doc))
-            .catch(err => res.json(err));
+        newUser.save().then(user => {
+            sendJwt(user, res);
+        });
     },
 
     updateProfile: async (req, res) => {
@@ -103,4 +82,30 @@ module.exports = {
             .then(doc => res.json(doc))
             .catch(err => res.json(err));
     }
+};
+
+//private
+
+sendJwt = (user, res) => {
+    let token = jwt.sign(
+        {
+            data: {
+                email: user.email,
+                password: user.password,
+                _id: user._id
+            }
+        },
+        "secret",
+        { expiresIn: "365 days" }
+    );
+
+    const userInfo = {
+        email: user.email,
+        userType: user.userType,
+        _id: user._id,
+        profilePic: user.profilePic,
+        phone: user.phoneNumber
+    };
+
+    res.json({ userInfo, token: token });
 };
