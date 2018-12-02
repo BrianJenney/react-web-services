4aa57afae95c0cb03138ad951dd085e770aa1fbf
BrianJenney <brianjenney83@gmail.com>
2018-12-01 19:49:56 -0800

    console logging for tests

--- models/userSchema.js
+++ models/userSchema.js
@@ -59,6 +59,7 @@ const UserSchema = new Schema({
 
 // hash the password
 UserSchema.methods.generateHash = function(password) {
+    console.log("hashing password");
     return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
 };
 
