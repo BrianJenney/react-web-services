a093ebc9699c3d8ae65a20ec6dfaee513a09cf67
BrianJenney <brianjenney83@gmail.com>
2020-08-18 19:56:37 -0700

    add post route to controller to get favorites

--- routes/api/property.js
+++ routes/api/property.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const graphqlHTTP = require('express-graphql');
 const propertySchema = require('../../graphqlModels/property');
 const propertyController = require('../../controllers/propertyController');
+const verify = require('../auth/authVerify.js');
 const multipart = require('connect-multiparty');
 const multipartMiddleware = multipart();
 
@@ -18,6 +19,8 @@ router.route('/searchlistings').post(propertyController.searchListings);
 router.route('/info/:id').get(propertyController.houseInfo);
 
 router.route('/disclosure').post(multipartMiddleware, propertyController.uploadDocument);
+
+router.route('/favorites').post(verify, propertyController.getFavoritedProperties);
 
 router.use(
 	'/graphql',
