9bdcb18040273945e60993429af611af645039bc
BrianJenney <brianjenney83@gmail.com>
2020-09-13 13:38:29 -0700

    add prettier and allow OPTIONS header

--- server.js
+++ server.js
@@ -17,11 +17,14 @@ const app = express();
 app.use(cors());
 
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	res.header('Access-Control-Allow-Headers', 'Content-Type');
-	next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,13 +35,13 @@ app.listen(process.env.PORT || 8081);
 app.use(routes);
 
 socket.on('connection', (socket) => {
-	console.log(`User on socket ${socket}`);
+    console.log(`User on socket ${socket}`);
 });
 
 socket.on('disconnect', () => {
-	console.log('user disconnected');
+    console.log('user disconnected');
 });
 
 module.exports = {
-	socket
+    socket,
 };
